fec182d9321b8f6d0f64feacb502f4e9d307619c
Tim Biddington <timbiddington@fastmail.com.au>
2020-12-17 11:14:00 +1100

    init

---
+++ convex-hull.js
@@ -0,0 +1,57 @@
+function convexHull(points) {
+  let lh = lowerHull( points );
+  let uh = upperHull( points );
+
+  return [ ...uh, ...lh.slice(0, lh.length) ];
+}
+
+function lowerHull(points) {
+  // Sort our points l->r so we can pick them out
+  const sortedPoints = points.sort((p, q) => (p.x < q.x));
+
+  // The list of points in the convex hull
+  let boundary = [];
+
+  // Add the first two points to kick off our chain
+  boundary.push(sortedPoints[0]);
+  boundary.push(sortedPoints[1]);
+
+  // Test each point
+  for (let i = 2; i < sortedPoints.length; i++) {
+    // Push our candidate onto the chain
+    boundary.push(sortedPoints[i]);
+
+    // While the last 3 points make a left turn, remove the 2nd-to-last
+    while(boundary.length > 2 && leftTurn(boundary)) {
+      boundary = removeNthFromTail(2, boundary);
+    }
+  }
+
+  return boundary;
+}
+
+function upperHull(points) {
+
+  // Sort our points l->r so we can pick them out
+  const sortedPoints = points.sort((p, q) => (p.x > q.x));
+
+  // The list of points in the convex hull
+  let boundary = [];
+
+  // Add the first two points to kick off our chain
+  boundary.push(sortedPoints[0]);
+  boundary.push(sortedPoints[1]);
+
+  // Test each point
+  for (let i = 2; i < sortedPoints.length; i++) {
+    // Push our candidate onto the chain
+    boundary.push(sortedPoints[i]);
+
+    // While the last 3 points make a left turn, remove the 2nd-to-last
+    while(boundary.length > 2 && leftTurn(boundary)) {
+      boundary = removeNthFromTail(2, boundary);
+    }
+  }
+
+  return boundary;
+}
