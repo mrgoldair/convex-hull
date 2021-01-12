7e0fee03fb061320461fe0375ca260d8203f869c
Tim Biddington <timbiddington@fastmail.com.au>
2021-01-13 08:00:56 +1100

    Housekeeping

--- list.js
+++ list.js
@@ -1,16 +1,8 @@
-// Removes nth element from tail
-export const removeNthFromTail = (n, xs) => {
-  // Copy our input array
-  return [ ...xs.slice(0, xs.length - n), ...xs.slice(xs.length - (n - 1)) ]
-  let res = Array.from(xs);
 
-  // Splice works in-place
-  res.splice(res.length - n, 1);
-  return res;
+export const removeNthFromTail = (n, xs) => {
+  return [ ...xs.slice(0, xs.length - n), ...xs.slice(xs.length - (n - 1)) ]
 }
 
-// Get last n elements from xs
 export const lastN = n => xs => (xs.slice(xs.length - n))
 
-// Last 1
 export const last = lastN(1);
